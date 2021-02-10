d096b4621ffa67f324ebe99e3c738a1e42174014
Seungwoo <seung9721@naver.com>
2021-02-10 11:49:34 +0900

    Button working

--- DataLoadTest/Global/DB.cs
+++ DataLoadTest/Global/DB.cs
@@ -9,23 +9,26 @@ namespace DataLoadTest.Global
     public class DB
     {
         // 1. DB 연결  
-        string connStr = "user id=DEV_ORA_TEST;password=DEV_ORA_TEST;" +
+        string connStr { get; set; } = "user id=DEV_ORA_TEST;password=DEV_ORA_TEST;" +
         "data source=(DESCRIPTION=(ADDRESS=" +
         "(PROTOCOL=tcp)(HOST=192.168.0.110)" +
         "(PORT=1521))(CONNECT_DATA=" +
         "(SID=orcl)))";
 
         //OracleConnection conn = new OracleConnection(connStr);
-        
+
 
 
         public void AAA()
         {
-
+            
             OracleConnection conna = new OracleConnection(connStr);
             conna.Open();
-            System.Console.WriteLine("QWEQWEQWE");
+            System.Console.WriteLine("TTTTEEEEESSSSSTTTT");
+            
+            
         }
 
+        
     }
 }
